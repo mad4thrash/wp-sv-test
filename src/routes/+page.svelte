7a823e532e97fd8a6d326f6c09706896c77464c0
mad4thrash <fabio.plumeri@oisluxurygroup.com>
2024-03-29 15:09:44 +0100

    refactored formatDate function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,76 +1,84 @@
 <script>
-// @ts-nocheck
-    export let data;
-    console.log(data);
+	// @ts-nocheck
+	export let data;
+	console.log(data);
 
-    const formatDate = (date) => {
-        if (isNaN(date.getTime())){
-            return 'data non disponibile'
-        } 
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        return `${day}/${month}/${year} ${hours}:${minutes}`;
-    }
-
+	const formatDate = (date) => {
+        if(!date) return 'data non disponibile';
+		const day = date.getDate();
+		const month = date.getMonth() + 1;
+		const year = date.getFullYear();
+		const hours = date.getHours();
+		const minutes = date.getMinutes();
+		return `${day}/${month}/${year} ${hours}:${minutes}`;
+	};
 </script>
+
 {#await data}
-<div class="w-2/3 h-2/5 bg-slate-300 mx-auto mt-12" >
-    <p>Caricamento...</p>
-</div>
+	<div class="w-2/3 h-2/5 bg-slate-300 mx-auto mt-12">
+		<p>Caricamento...</p>
+	</div>
 {:then}
-<div class="w-2/3 h-2/5 bg-slate-300 mx-auto my-12" >
-      <table class="w-full bg-white shadow-lg">
-        <thead>
-            <tr>
-                <th class="border px-4 py-2">Ordine</th>
-                <th class="border px-4 py-2">Data</th>
-                <th class="border px-4 py-2">Cliente</th>
-                <th class="border px-4 py-2">Totale</th>
-                <th class="border px-4 py-2">PB</th>
-                <th class="border px-4 py-2">CV</th>
-                <th class="border px-4 py-2">Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each data.body.orders as order}
-                <tr>
-                    <td class="border px-4 py-2">{order.order_id}</td>
-                    <td class="border px-4 py-2">{formatDate(order.order_datetime)}</td>
-                    <td class="border px-4 py-2">{order.billing_first_name} {order.billing_last_name}</td>
-                    <td class="border px-4 py-2">€ {order.order_total}</td>
-                    <td class="border px-4 py-2">{order.pb_total}</td>
-                    <td class="border px-4 py-2">{order.cv_total}</td>
-                    <td class="border px-4 py-2">
-                    {#if order.status === 'completed'}
-                        <span class="text-green-400
-                        font-bold">Completato</span>
-                        {:else if order.status === 'shipped'}
-                        <span class="text-green-400
-                        font-bold">Spedito</span>
-                    {:else if order.status === 'processing'}
-                        <span class="text-blue-400
-                        font-bold">In lavorazione</span>
-                    {:else if order.status === 'pending' || order.status === 'on-hold'}
-                        <span class="text-yellow-400
-                        font-bold">In attesa</span>
-                    {:else if order.status === 'cancelled'}
-                        <span class="text-red-400
-                        font-bold">Annullato</span>
-                    {:else}
-                        <span class="text-red-600
-                        font-bold">Errore</span>
-                    {/if}
-                    </td>
-                </tr>
-
-                 
-               
-            {/each}
-        </tbody>
-    </table>
-</div>
+	<div class="w-2/3 h-2/5 bg-slate-300 mx-auto my-12">
+		<table class="w-full bg-white shadow-lg">
+			<thead>
+				<tr>
+					<th class="border px-4 py-2">Ordine</th>
+					<th class="border px-4 py-2">Data</th>
+                    <th class="border px-4 py-2">Pagato in data</th>
+					<th class="border px-4 py-2">Cliente</th>
+					<th class="border px-4 py-2">Totale</th>
+					<th class="border px-4 py-2">PB</th>
+					<th class="border px-4 py-2">CV</th>
+					<th class="border px-4 py-2">Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.body.orders as order}
+					<tr>
+						<td class="border px-4 py-2">{order.order_id}</td>
+						<td class="border px-4 py-2">{formatDate(order.order_datetime)}</td>
+                        <td class="border px-4 py-2">{formatDate(order.order_paid_at_datetime)}</td>
+						<td class="border px-4 py-2">{order.billing_first_name} {order.billing_last_name}</td>
+						<td class="border px-4 py-2">€ {order.order_total}</td>
+						<td class="border px-4 py-2">{order.pb_total}</td>
+						<td class="border px-4 py-2">{order.cv_total}</td>
+						<td class="border px-4 py-2">
+							{#if order.status === 'completed'}
+								<span
+									class="text-green-400
+                        font-bold">Completato</span
+								>
+							{:else if order.status === 'shipped'}
+								<span
+									class="text-green-400
+                        font-bold">Spedito</span
+								>
+							{:else if order.status === 'processing'}
+								<span
+									class="text-blue-400
+                        font-bold">In lavorazione</span
+								>
+							{:else if order.status === 'pending' || order.status === 'on-hold'}
+								<span
+									class="text-yellow-400
+                        font-bold">In attesa</span
+								>
+							{:else if order.status === 'cancelled'}
+								<span
+									class="text-red-400
+                        font-bold">Annullato</span
+								>
+							{:else}
+								<span
+									class="text-red-600
+                        font-bold">Errore</span
+								>
+							{/if}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
 {/await}
-
