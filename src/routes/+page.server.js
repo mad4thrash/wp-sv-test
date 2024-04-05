c0e1a90de1522455aa427ea5fa6216a91f96ab77
mad4thrash <fabio.plumeri@oisluxurygroup.com>
2024-04-05 11:50:51 +0200

    dates

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,19 +5,22 @@ export async function load() {
 	try {
 		const connection = await getDBConnection();
 		const ordersQuery = `
-			SELECT 
-    wc_ois.*,
-    MAX(CASE WHEN um.meta_key = 'billing_first_name' THEN um.meta_value END) AS billing_first_name,
-    MAX(CASE WHEN um.meta_key = 'billing_last_name' THEN um.meta_value END) AS billing_last_name
-FROM 
-    wp_wc_ois_cvandpb wc_ois
-LEFT JOIN 
-    wp_usermeta um ON wc_ois.customer_id = um.user_id
-AND 
-    um.meta_key IN ('billing_first_name', 'billing_last_name')
-GROUP BY 
-    wc_ois.id;
-		`;
+    SELECT 
+        wc_ois.*,
+        MAX(CASE WHEN um.meta_key = 'billing_first_name' THEN um.meta_value END) AS billing_first_name,
+        MAX(CASE WHEN um.meta_key = 'billing_last_name' THEN um.meta_value END) AS billing_last_name,
+        ucode.influ_code
+    FROM 
+        wp_wc_ois_cvandpb wc_ois
+    LEFT JOIN 
+        wp_usermeta um ON wc_ois.customer_id = um.user_id
+    LEFT JOIN 
+        wp_wc_ois_users_codes ucode ON wc_ois.customer_id = ucode.user_id
+    WHERE 
+        um.meta_key IN ('billing_first_name', 'billing_last_name')
+    GROUP BY 
+        wc_ois.id, ucode.influ_code;
+`;
 		const [orders] = await connection.execute(ordersQuery);
 
 		connection.end();
